c52785fb419588d64a3d4ee5f3bd7bf4f6a8d945
doxxx <doxxx93@gmail.com>
2023-03-30 03:05:01 +0900

    feat:스웨거 적용

--- src/main/kotlin/com/beside/daldal/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/beside/daldal/domain/comment/controller/CommentController.kt
@@ -5,6 +5,11 @@ import com.beside.daldal.domain.comment.dto.CommentDTO
 import com.beside.daldal.domain.comment.dto.CommentUpdateDTO
 import com.beside.daldal.domain.comment.service.CommentService
 import com.beside.daldal.domain.review.service.ReviewService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.security.Principal
@@ -15,7 +20,26 @@ class CommentController(
     private val commentService: CommentService,
     private val reviewService: ReviewService
 ) {
-
+    // swagger
+    @Operation(
+        operationId = "createComment",
+        summary = "댓글 생성",
+        description = "댓글을 생성합니다. 댓글을 생성하면 해당 리뷰에 댓글이 추가됩니다.",
+        tags = ["comment"]
+    )
+    @ApiResponses(
+        value =
+        [
+            ApiResponse(
+                responseCode = "200",
+                description = "success",
+                content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = CommentDTO::class)
+                )]
+            ),
+        ]
+    )
     @PostMapping("/{reviewId}")
     fun createComment(
         principal: Principal,
@@ -27,6 +51,25 @@ class CommentController(
         return ResponseEntity.ok(commentDTO)
     }
 
+    @Operation(
+        operationId = "deleteComment",
+        summary = "댓글 삭제",
+        description = "댓글을 삭제합니다. 댓글을 삭제하면 해당 리뷰에 댓글이 삭제됩니다.",
+        tags = ["comment"]
+    )
+    @ApiResponses(
+        value =
+        [
+            ApiResponse(
+                responseCode = "200",
+                description = "success",
+                content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = String::class)
+                )]
+            ),
+        ]
+    )
     @DeleteMapping("/{reviewId}/{commentId}")
     fun deleteComment(
         principal: Principal,
@@ -38,6 +81,25 @@ class CommentController(
         return ResponseEntity.ok(commentId)
     }
 
+    @Operation(
+        operationId = "updateComment",
+        summary = "댓글 수정",
+        description = "댓글을 수정합니다. 댓글을 수정하면 해당 리뷰에 댓글이 수정됩니다.",
+        tags = ["comment"]
+    )
+    @ApiResponses(
+        value =
+        [
+            ApiResponse(
+                responseCode = "200",
+                description = "success",
+                content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = CommentDTO::class)
+                )]
+            ),
+        ]
+    )
     @PutMapping("/{reviewId}/{commentId}")
     fun updateComment(
         principal: Principal,
