69a8bee1aad897166a9f68ce01eb7c67971420e6
tianea2160 <rhlehfndvkd7557@daum.net>
2023-03-30 12:19:31 +0900

    feat : 리뷰 필터링 및 페이지네이션 구현

--- src/main/kotlin/com/beside/daldal/domain/review/repository/ReviewRepository.kt
+++ src/main/kotlin/com/beside/daldal/domain/review/repository/ReviewRepository.kt
@@ -1,10 +1,18 @@
 package com.beside.daldal.domain.review.repository
 
 import com.beside.daldal.domain.review.entity.Review
+import com.beside.daldal.domain.review.entity.ReviewFeature
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ReviewRepository :MongoRepository<Review, String>, CustomReviewDSL {
-    fun findAllByMemberId(id: String) : List<Review>
+interface ReviewRepository : MongoRepository<Review, String>, CustomReviewDSL {
+    fun findAllByMemberId(id: String): List<Review>
+
+    @Query("{ features :  { \$in : ?0 }}")
+    fun findAllByFilter(features: List<ReviewFeature>, pageable: Pageable): Page<Review>
 }
