b3df44a190dc682d33cebc9de0ab5ab1fe3b5eff
tianea2160 <rhlehfndvkd7557@daum.net>
2023-03-30 11:15:55 +0900

    fix : course update, delete 권한 검사 로직 추가 및 리팩토링

--- src/main/kotlin/com/beside/daldal/domain/course/dto/CourseUpdateDTO.kt
+++ src/main/kotlin/com/beside/daldal/domain/course/dto/CourseUpdateDTO.kt
@@ -5,15 +5,14 @@ import com.beside.daldal.domain.course.entity.Course
 
 class CourseUpdateDTO(
     val name: String,
-    val memberId: String,
     val distance: Long,
     val duration: Long,
     val points: List<Map<String, Any?>>
 ) {
-    fun toEntity(courseId: String): Course = Course(
+    fun toEntity(courseId: String, memberId: String): Course = Course(
         id = courseId,
-        name = name,
         memberId = memberId,
+        name = name,
         distance = distance,
         duration = duration,
         points = points
